7a977b65b78442485c5ed77230ab01bf39170d67
renxin <renxin2024@gmail.com>
2024-11-12 10:14:03 +0800

    feat:修改菜单

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -1,26 +1,26 @@
-import { sidebar } from "vuepress-theme-hope";
+import {sidebar} from "vuepress-theme-hope";
 
 export default sidebar({
-  "/": [
-    "",
-    {
-      text: "如何使用",
-      icon: "laptop-code",
-      prefix: "demo/",
-      link: "demo/",
-      children: "structure",
-    },
-    {
-      text: "文章",
-      icon: "book",
-      prefix: "posts/",
-      children: "structure",
-    },
-    "intro",
-    {
-      text: "幻灯片",
-      icon: "person-chalkboard",
-      link: "https://ecosystem.vuejs.press/zh/plugins/markdown/revealjs/demo.html",
-    },
-  ],
+    "/": [
+        "",
+        {
+            text: "如何使用",
+            icon: "laptop-code",
+            prefix: "demo/",
+            link: "demo/",
+            children: "structure",
+        },
+        {
+            text: "文章阅读",
+            icon: "book",
+            prefix: "posts/",
+            children: "structure",
+        },
+        "intro",
+        {
+            text: "幻灯片",
+            icon: "person-chalkboard",
+            link: "https://ecosystem.vuejs.press/zh/plugins/markdown/revealjs/demo.html",
+        },
+    ],
 });
